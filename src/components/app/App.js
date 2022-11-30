de6f04a199074aca2fb7aef079cca9d35fb91865
Your Name <romancazac999@gmail.com>
2022-11-30 15:47:53 +0200

    init

---
+++ src/components/app/App.js
@@ -0,0 +1,33 @@
+import { Route, Routes} from 'react-router-dom';
+
+import {MainPage, ComicsPage} from '../pages';
+import AppHeader from "../appHeader/AppHeader";
+
+const App = () => {
+    
+    return (
+        
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes> 
+                    
+                        <Route exact path="/"
+                        element={<MainPage/>}
+                        >
+                            
+                        </Route>
+                        <Route  exact path="/comics"
+                        element={<ComicsPage/>}
+                        >
+                            
+                        </Route>
+                  
+                </Routes>
+                </main>
+            </div>
+       
+    )
+}
+
+export default App;
---
+++ src/components/appHeader/AppHeader.js
@@ -0,0 +1,30 @@
+import {Link, NavLink} from 'react-router-dom';
+import './appHeader.scss';
+
+const AppHeader = () => {
+    let activeStyle = {
+        color: "#9f0013",
+      };
+    return (
+        <header className="app__header">
+            <h1 className="app__title">
+                <Link to="/">
+                    <span>Marvel</span> information portal
+                </Link>
+            </h1>
+            <nav className="app__menu">
+                <ul>
+                    <li><NavLink  style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                        } to="/">Characters</NavLink></li>
+                    /
+                    <li><NavLink  style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            } to="/comics">Comics</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default AppHeader;
